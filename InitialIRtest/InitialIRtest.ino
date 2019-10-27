dfdf7caa2146566f3ce09ad39c7dae190bf9c61f
Iván Morales <ivan.rene.morales@gmail.com>
2019-10-27 00:28:18 -0600

    Initial IR test
    Sucessfully woke vaccum robot on via IR LED

---
+++ InitialIRtest/InitialIRtest.ino
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <IRsend.h>
+
+const uint16_t IR_LED = D13;
+IRsend irsend(IR_LED);
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(IR_LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  irsend.sendNEC(0x02aa22dd);
+  delay(500);
+}
